1b34aa526f10365b6314f361aca17fe7ca3deaa4
chisela14 <chisela14@gmail.com>
2022-03-07 11:37:50 +0100

    coreccion 2 y 3

---
+++ Consultas/ConsultaGest1.sql
@@ -0,0 +1,37 @@
+--1. Código, fecha y doble del descuento de las facturas con iva cero.
+--2. Código de las facturas con iva nulo.
+--3. Código y fecha de las facturas sin iva (iva cero o nulo).
+--4. Código de factura y de artículo de las líneas de factura en las que la cantidad solicitada es menor de 5
+--unidades y además se ha aplicado un descuento del 25% o mayor.
+--5. Obtener la descripción de los artículos cuyo stock está por debajo del stock mínimo, dado también la
+--cantidad en unidades necesaria para alcanzar dicho mínimo.
+--6. Ivas distintos aplicados a las facturas.
+--7. Código, descripción y stock mínimo de los artículos de los que se desconoce la cantidad de stock. Cuando
+--se desconoce la cantidad de stock de un artículo, el stock es nulo.
+--8. Obtener la descripción de los artículos cuyo stock es más de tres veces su stock mínimo y cuyo precio
+--supera los 6 euros.
+--9. Código de los artículos (sin que salgan repetidos) comprados en aquellas facturas cuyo código está entre 8 y 10.
+--10. Mostrar el nombre y la dirección de todos los clientes.
+11. Mostrar los distintos códigos de pueblos en donde tenemos clientes
+12. Obtener los códigos de los pueblos en donde hay clientes con código de cliente menor que el código
+25. No deben salir códigos repetidos.
+13. Nombre de las provincias cuya segunda letra es una 'O' (bien mayúscula o minúscula).
+14. Código y fecha de las facturas del año pasado para aquellos clientes cuyo código se encuentra entre
+50 y 100.
+15. Nombre y dirección de aquellos clientes cuyo código postal empieza por “12”.
+16. Mostrar las distintas fechas, sin que salgan repetidas, de las factura existentes de clientes cuyos
+códigos son menores que 50.
+17. Código y fecha de las facturas que se han realizado durante el mes de junio del año 2004
+18. Código y fecha de las facturas que se han realizado durante el mes de junio del año 2004 para
+aquellos clientes cuyo código se encuentra entre 100 y 250.
+19. Código y fecha de las facturas para los clientes cuyos códigos están entre 90 y 100 y no tienen iva.
+NOTA: una factura no tiene iva cuando éste es cero o nulo.
+20. Nombre de las provincias que terminan con la letra 's' (bien mayúscula o minúscula).
+21. Nombre de los clientes cuyo código postal empieza por “02”, “11” ó “21”.
+22. Artículos (todas las columnas) cuyo stock sea mayor que el stock mínimo y no haya en stock más de
+5 unidades del stock_min.
+23. Nombre de las provincias que contienen el texto “MA” (bien mayúsculas o minúsculas).
+24. Se desea promocionar los artículos de los que se posee un stock grande. Si el artículo es de más de
+6000 € y el stock supera los 60000 €, se hará un descuento del 10%. Mostrar un listado de los artículos
+que van a entrar en la promoción, con su código de artículo, nombre del articulo, precio actual y su precio
+en la promoción.
